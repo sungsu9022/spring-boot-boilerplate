5d58e6adfbb0f2eb71f879efb051ebcc3320d66f
sungsu.park <sungsu.park@navercorp.com>
2018-11-28 03:32:41 +0900

    [#1] set jpa configuration

--- src/main/java/com/sungsu/boilerplate/config/PersistenceConfig.java
+++ src/main/java/com/sungsu/boilerplate/config/PersistenceConfig.java
@@ -1,7 +1,12 @@
 package com.sungsu.boilerplate.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
@@ -14,28 +19,34 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
+import javax.validation.Valid;
+import java.util.Properties;
 
 @Configuration
 @EnableJpaRepositories
 @EnableTransactionManagement
 public class PersistenceConfig {
+
 	@Bean
 	public DataSource dataSource() {
 		EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
 		return builder.setType(EmbeddedDatabaseType.H2).build();
 	}
 
+
 	@Bean
 	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
 
 		HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
 		vendorAdapter.setGenerateDdl(true);
+		vendorAdapter.setShowSql(Boolean.valueOf(jpaProperties().isShowSql()));
 
 		LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
 		factory.setJpaVendorAdapter(vendorAdapter);
 		factory.setPackagesToScan("com.sungsu.boilerplate.domain");
 		factory.setDataSource(dataSource());
 		factory.setJpaDialect(new DefaultJpaDialect());
+		factory.setJpaProperties(makeJpaProperties());
 		return factory;
 	}
 
@@ -44,6 +55,22 @@ public class PersistenceConfig {
 		JpaTransactionManager txManager = new JpaTransactionManager();
 		txManager.setEntityManagerFactory(entityManagerFactory);
 		return txManager;
+	}
+
+	@Bean
+	public JpaProperties jpaProperties() {
+		return new JpaProperties();
+	}
+
+	private Properties makeJpaProperties() {
+		Properties properties = new Properties();
+		properties.setProperty("hibernate.hbm2ddl.auto", jpaProperties().getHibernate().getDdlAuto());
+		properties.setProperty("hibernate.dialect", jpaProperties().getDatabasePlatform());
+		properties.setProperty("hibernate.use_sql_comments", jpaProperties().getProperties().get("hibernate.use_sql_comments"));
+		properties.setProperty("hibernate.naming.physical-strategy", jpaProperties().getHibernate().getNaming().getPhysicalStrategy());
+		properties.setProperty("hibernate.naming.implicit-strategy", jpaProperties().getHibernate().getNaming().getImplicitStrategy());
+		properties.setProperty("hibernate.hbm2ddl.auto", jpaProperties().getHibernate().getDdlAuto());
+		return properties;
 	}
 
 }
